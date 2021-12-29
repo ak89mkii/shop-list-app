b882171f477f67bc5b7792fec2e8946dc3c4f29a
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2021-12-29 17:02:14 -0500

    Added Pressable components.

--- components/List.js
+++ components/List.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable } from 'react-native';
+// import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const List = ({item}) => {
   return (
-    <TouchableOpacity>
-    <View style={styles.list}>
-      <Text style={styles.listView}>{item.text}</Text>
+    <TouchableOpacity style={styles.list}>
+    <View style={styles.listView}>
+        <Text style={styles.listView}>{item.text}</Text>
+        <Pressable style={styles.listViewButtonAdd}>
+            <Text style={styles.listViewButtonAddText}>Added to Cart</Text>
+        </Pressable>
+        <Pressable style={styles.listViewButtonAdd2}>
+            <Text style={styles.listViewButtonAddText2}>Remove</Text>
+        </Pressable>
     </View>
     </TouchableOpacity>
   );
@@ -19,10 +26,51 @@ const styles = StyleSheet.create({
         borderColor: '#eee',
     },
     listView: {
+        fontSize: 10,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
         flexDirection: 'row',
         color: 'black',
         fontSize: 10,
         justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    listViewButtonAdd: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'orange',
+        width: 100,
+        height: 30,
+    },
+    listViewButtonAddText: {
+        fontSize: 10,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+    listViewButtonAdd2: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: 'black',
+        width: 70,
+        height: 30,
+    },
+    listViewButtonAddText2: {
+        fontSize: 10,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
     },
 });
 
